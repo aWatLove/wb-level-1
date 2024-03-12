b37d640c2e5b160975419e68dabf3ae34bcb464b
Vladislav <watislove4u@gmail.com>
2024-03-12 23:09:25 +0400

    comment tasks 1-4

--- tasks/task1/composition.go
+++ tasks/task1/composition.go
@@ -9,17 +9,20 @@ import (
 	Реализовать встраивание методов в структуре Action от родительской структуры
 	Human (аналог наследования).
 */
-/* Объявляем структуру human*/
+
+/* структуру human*/
 type human struct {
 	Name    string
 	Surname string
 	age     int
 }
 
+// конструктор структуры human
 func newHuman(name string, surname string, age int) *human {
 	return &human{Name: name, Surname: surname, age: age}
 }
 
+// метод структуры human для проверки возраста
 func (h human) isAdult() bool {
 	if h.age < 18 {
 		return false
@@ -28,18 +31,22 @@ func (h human) isAdult() bool {
 	}
 }
 
+// метод структуры human
 func (h human) hello() {
 	fmt.Printf("Hello, my name is %s\n", h.Name)
 }
 
-// объявляем структуру action
+// структура action
 type action struct {
 	*human // встраиваем human
 }
 
 func main() {
-	h := newHuman("Steve", "Stevenson", 21)
-	act := action{h}
+	h := newHuman("Steve", "Stevenson", 21) // объявляем объект структуры human
+
+	act := action{h} // объявляем объект структуры action
+
+	// вызываем методы human у объекта структуры action
 	fmt.Println(act.isAdult())
 	act.hello()
 }
--- tasks/task4/workers.go
+++ tasks/task4/workers.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 /*
 Реализовать постоянную запись данных в канал (главный поток). Реализовать
 набор из N воркеров, которые читают произвольные данные из канала и
@@ -10,6 +19,53 @@ package main
 способ завершения работы всех воркеров.
 */
 
-func main() {
+// функция, которая будет запускаться в качестве горутины
+func worker(id int, ch <-chan int, stop <-chan os.Signal) {
+	for {
+		select {
+		case <-stop:
+			fmt.Printf("stop working goroutine with id=%d\n", id)
+			return
+		case val := <-ch:
+			fmt.Printf("goroutine id=%d value=%d\n", id, val)
+		}
+	}
+}
 
+// Кол-во горутин
+const workerAmount = 5
+
+func main() {
+	// Объявление буферизированного канала для данных
+	// размер буфера равен кол-ву горутин
+	ch := make(chan int, workerAmount)
+	defer close(ch) // объявляем об отложенном закрытии канала
+
+	mainSignCh := make(chan os.Signal, 1) // буферизированный канал для сигнала
+
+	// Объявляем какие сигналы "слушать" методом Notify, чтобы передать об этом в наш канал mainSignCh
+	signal.Notify(mainSignCh, syscall.SIGTERM, syscall.SIGINT)
+
+	// Запуск горутин в цикле,
+	// а также создание сигнального канала для каждой горутины
+	for i := 0; i < workerAmount; i++ {
+		signCh := make(chan os.Signal, 1)
+		signal.Notify(signCh, syscall.SIGTERM, syscall.SIGINT)
+
+		go worker(i, ch, signCh)
+	}
+
+	// основной цикл main
+	for {
+		// по-умолчанию записываются новые данные в канал, либо при получении сигнала, поток останавливается
+		select {
+		case <-mainSignCh:
+			fmt.Println("main is stopped")
+			return
+		default:
+			ch <- rand.Int()
+			time.Sleep(time.Millisecond * 100)
+		}
+
+	}
 }
